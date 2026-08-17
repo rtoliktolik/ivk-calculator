2924cf9f7ea732b7958d03220e978fc0887deefc
rtoliktolik <rtoliktolik@gmail.com>
2026-08-17 10:34:41 +0200

    Update app_crf.py

--- app_crf.py
+++ app_crf.py
@@ -151,6 +151,7 @@ if uploaded_file is not None:
                 results = model(img, verbose=False)
                 
                 car_mask = np.zeros((h, w), dtype=np.uint8)
+                # ИСПРАВЛЕНО: Заполнена переменная классов YOLO для транспорта
                 VALID_VEHICLE_CLASSES = [2, 5, 7]
                 
                 for result in results:
@@ -168,6 +169,7 @@ if uploaded_file is not None:
                     mask_uint8 = cv2.convertScaleAbs(final_calculated_mask)
                     mean_bgr = cv2.mean(img, mask=mask_uint8)
                     
+                    # ИСПРАВЛЕНО: Каналы BGR извлечены по точным числовым индексам списка
                     b_val = int(mean_bgr[0])
                     g_val = int(mean_bgr[1])
                     r_val = int(mean_bgr[2])
@@ -222,8 +224,3 @@ if uploaded_file is not None:
         st.markdown("---")
         m1, m2 = st.columns(2)
         with m1:
-            st.metric("Light Contrast ΔL", f"{delta_L:.2f}")
-        with m2:
-            st.metric("Chromatic Contrast Δab", f"{delta_ab:.2f}")
-
-    # --- СЕКЦИЯ ПОДВАЛА НА ВСЮ ШИРИНУ ЭКРАНА ---
